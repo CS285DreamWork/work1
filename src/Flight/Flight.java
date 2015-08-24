676ab6028e6c8c439e3556bbdb326df7929049e0
Joy <Joy@Joy-HP>
2015-08-24 20:22:14 +0700

    สร้างคลาส Flight

--- src/Flight/Flight.java
+++ src/Flight/Flight.java
@@ -5,11 +5,58 @@
  */
 package Flight;
 
+import java.text.DateFormat;
+
 /**
  *
  * @author Joy
  */
 public class Flight{
 
+    String to;
+    String from;
+    DateFormat departTime;
+    DateFormat departDate;
+    DateFormat returnDate;
+    DateFormat returnTime;
+    Boolean destination;
+    Boolean directFight;
+    int travellers;
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setDepartTime(DateFormat departTime) {
+        this.departTime = departTime;
+    }
+
+    public void setDepartDate(DateFormat departDate) {
+        this.departDate = departDate;
+    }
+
+    public void setReturnDate(DateFormat returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public void setReturnTime(DateFormat returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public void setDestination(Boolean destination) {
+        this.destination = destination;
+    }
+
+    public void setDirectFight(Boolean directFight) {
+        this.directFight = directFight;
+    }
+
+    public void setTravellers(int travellers) {
+        this.travellers = travellers;
+    }
     
 }
